15db040733b3ee311e7811244b86cfbb086543ac
coffeerumba <coffeerumba@users.noreply.github.com>
2024-09-29 01:49:23 +0900

    Update createNotionPageByTweet.test.ts

--- src/createNotionPageByTweet.test.ts
+++ src/createNotionPageByTweet.test.ts
@@ -20,7 +20,7 @@ const server = setupServer(
       })
     );
   }),
-  rest.post("https://api.notion.com/v1/databases/:databaseId/query", async (req, res, ctx) => {
+  rest.post("https://api.notion.com/v1/databases/:database_id/query", async (req, res, ctx) => {
     const { databaseId } = req.params;
     const body = await req.json();
     mockQueryFn(body, databaseId);
