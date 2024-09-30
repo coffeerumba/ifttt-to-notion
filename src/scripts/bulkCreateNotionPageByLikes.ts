afb52ff6ef9ea012605c4e5bcae2beee7606c2eb
coffeerumba <coffeerumba@users.noreply.github.com>
2024-09-30 11:33:25 +0900

    Update bulkCreateNotionPageByLikes.ts

--- src/scripts/bulkCreateNotionPageByLikes.ts
+++ src/scripts/bulkCreateNotionPageByLikes.ts
@@ -9,11 +9,12 @@ import { createNotionPageByTweet } from "../createNotionPageByTweet";
 
 const fileName = process.argv[2] || "likes.js";
 const tweetsJson = require(path.resolve(fileName));
+tweetsJson.reverse();
 
 async function addTweetsToNotionDatabase(tweets: any[]) {
   for (const tweet of tweets) {
     await addTweetToNotionDatabase(tweet);
-    await new Promise((resolve) => setTimeout(resolve, 334)); // Limit to 3 requests per second, send requests at intervals of 334ms
+    await new Promise((resolve) => setTimeout(resolve, 500)); // Limit to 3 requests per second, send requests at intervals of 334ms
   }
 }
 
@@ -22,14 +23,19 @@ addTweetsToNotionDatabase(tweetsJson);
 async function addTweetToNotionDatabase(tweet: any) {
   try {
     const response = await createNotionPageByTweet({
-      text: tweet.like.fullText,
-      userName: "",
-      linkToTweet: tweet.like.expandedUrl,
-      firstLinkUrl: "",
-      embed: "",
+      text: tweet.tweet_content,
+      userName: tweet.user_handle,
+      linkToTweet: `https://twitter.com/${tweet.user_handle}/status/${tweet.tweet_id}`,
+      firstLinkUrl: tweet.tweet_media_urls.length ? tweet.tweet_media_urls[0] : "",
+      createdAt: tweet.tweet_created_at,
+      embed: `<blockquote class="twitter-tweet">
+ <p lang="en" dir="ltr">${tweet.tweet_content}</p>
+ &mdash; ${tweet.user_name} (@${tweet.user_handle})
+ <a href="https://twitter.com/${tweet.user_handle}/status/${tweet.tweet_id}">${tweet.tweet_created_at}</a>
+</blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
     });
-
-    console.log("New page created:", response.id, tweet.like.tweetId);
+    console.log("New page created:", response.id, tweet.tweet_id);
   } catch (error: any) {
     if (error.code) {
       console.error("API error:", error.code, error.message);
